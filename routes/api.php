41c1871b04aabda51a5230a5bf33042b86a70980
Riccardo Petricca <3359943+rc2pc2@users.noreply.github.com>
2024-08-27 01:42:11 +0200

    removed typo in kernel middlewares

--- routes/api.php
+++ routes/api.php
@@ -22,18 +22,19 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
-// Route::get("/posts", [PostController::class, "index"] )->name('posts.index');
-// Route::get("/posts/{post}", [PostController::class, "show"] )->name('posts.show');
+Route::middleware("custom.api.auth")->name('api.')->group(function(){
+    Route::get("/posts", [PostController::class, "index"] )->name('posts.index');
+    Route::get("/posts/{post}", [PostController::class, "show"] )->name('posts.show');
 
-// Route::get("/users", [UserController::class, "index"] )->name('users.index');
-// Route::get("/users/search", [UserController::class, "userSearch"] )->name('users.search');
-// Route::get("/users/{user}", [UserController::class, "show"] )->name('users.show');
+    Route::get("/users", [UserController::class, "index"] )->name('users.index');
+    Route::get("/users/search", [UserController::class, "userSearch"] )->name('users.search');
+    Route::get("/users/{user}", [UserController::class, "show"] )->name('users.show');
 
-// Route::middleware("custom.api.auth")->name('api.')->group(function(){
-//     // Route::get("/categories", [CategoryController::class, "index"] )->name('categories.index');
-//     // Route::get("/categories/{category}", [CategoryController::class, "show"] )->name('categories.show');
 
-//     Route::patch("/categories/{category}/restore", [CategoryController::class, "restore"] )->name('categories.restore');
-//     Route::delete("/categories/{category}/permanent-delete", [CategoryController::class, "permanentDestroy"] )->name('categories.permanent-destroy');
-//     Route::resource("/categories", CategoryController::class)->except(["create", "edit"]);
-// });
+    // Route::get("/categories", [CategoryController::class, "index"] )->name('categories.index');
+    // Route::get("/categories/{category}", [CategoryController::class, "show"] )->name('categories.show');
+
+    Route::patch("/categories/{category}/restore", [CategoryController::class, "restore"] )->name('categories.restore');
+    Route::delete("/categories/{category}/permanent-delete", [CategoryController::class, "permanentDestroy"] )->name('categories.permanent-destroy');
+    Route::resource("/categories", CategoryController::class)->except(["create", "edit"]);
+});
